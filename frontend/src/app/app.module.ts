cd08a4d258dd54e5334c3947d0c8b6b92c4db285
rybitew <rybitew@gmail.com>
2019-11-05 00:24:07 +0100

    Added flex layout Added vertical Menu

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {MenuComponent} from './menu/menu.component';
 import {RouterModule, Routes} from '@angular/router';
-import { UploadComponent } from './upload/upload.component';
-import { ManageAccountComponent } from './manage-account/manage-account.component';
-import { UserLibraryComponent } from './user-library/user-library.component';
-import { BookBrowserComponent } from './book-browser/book-browser.component';
+import {UploadComponent} from './upload/upload.component';
+import {ManageAccountComponent} from './manage-account/manage-account.component';
+import {UserLibraryComponent} from './user-library/user-library.component';
+import {BookBrowserComponent} from './book-browser/book-browser.component';
 import {MatListModule} from '@angular/material';
 
 const appRoutes: Routes = [
@@ -23,6 +24,7 @@ const appRoutes: Routes = [
   {path: 'books/categories', component: BookBrowserComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,9 +41,11 @@ const appRoutes: Routes = [
     MatButtonModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes, {enableTracing: true}),
-    MatListModule
+    MatListModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
