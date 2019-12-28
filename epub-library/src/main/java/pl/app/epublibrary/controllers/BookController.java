421e7fe0aba9db6e91c0d3d21d7bc22b9923c8cd
rybitew <rybitew@gmail.com>
2019-12-28 17:10:21 +0100

    Added controllers

---
+++ epub-library/src/main/java/pl/app/epublibrary/controllers/BookController.java
@@ -0,0 +1,7 @@
+package pl.app.epublibrary.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+}
---
+++ epub-library/src/main/java/pl/app/epublibrary/services/AdminService.java
@@ -0,0 +1,7 @@
+package pl.app.epublibrary.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+}
